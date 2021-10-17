22c065a8eb393d3300302bba6d811a812076541b
BulaninaNastya <90156756+BulaninaNastya@users.noreply.github.com>
2021-10-17 19:20:36 +0300

    Add files via upload

---
+++ Unit_test/Division/C#/Division.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Division
+{
+    class Division
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестировать функции деления двух чисел используя оператор исключения.");
+            Console.WriteLine("Справка: при делении на ноль типов с плавающей точкой никогда не генерируется исключение,");
+            Console.WriteLine("вместо этого всегда возращается значение, по умолчанию описанное в стандарте, то есть infinity.");
+            try
+            {
+                Console.WriteLine("Введите делимое");               
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите делитель");
+                int b = int.Parse(Console.ReadLine());
+                double s = a / b;
+                Console.WriteLine(s);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка. На ноль делить нельзя!");
+            }
+            catch 
+            {
+                Console.WriteLine("Ошибка");
+            }
+            finally
+            {
+                Console.WriteLine("Размер типа данных int равен " + sizeof(int) + " bytes");
+                Console.WriteLine("Размер типа данных float равен " + sizeof(float) + " bytes");
+                Console.WriteLine("Конец программы");
+            }
+        }
+    }
+}
